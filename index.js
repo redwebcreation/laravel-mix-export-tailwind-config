167bec53f65a481c0f19a4220a99ba2a4c963605
Félix Dorn <github@felixdorn.fr>
2021-03-19 13:24:29 +0100

    :bug: fix relative paths

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let fs = require('fs');
 let mix = require('laravel-mix');
 
-mix.extend('exportTailwindConfig', function (webpackConfig, configPath = './tailwind.config.js', exportPath = './storage/tailwind.config.json') {
-    let config = require(configPath);
+mix.extend('exportTailwindConfig', function (webpackConfig, configPath = 'tailwind.config.js', exportPath = 'storage/tailwind.config.json') {
+    const basePath = './../../'
+    let config = require(basePath + configPath)
 
     fs.writeFile(
-        exportPath,
+        basePath + exportPath,
         JSON.stringify(config, null, 2)
-    );
-});
+    )
+
+    return webpackConfig
+})
